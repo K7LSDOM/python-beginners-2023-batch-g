52870c3d513e13d058d7569a9338041ff2140bea
K7LSDOM <144117444+K7LSDOM@users.noreply.github.com>
2023-10-24 10:12:46 +0100

    Create classwork4.py

---
+++ Shedrack/classwork4.py
@@ -0,0 +1,25 @@
+# # Q1. Create 4 variables and add them all together and print the result
+# fruit1 = "mango"
+# fruit2 = "orange"
+# fruit3 = "watermelon"
+# fruit4 = "apple"
+# print(fruit1 + fruit2 + fruit3 + fruit4)
+
+
+# # Q2. Create a variable with the value 6 and update it to 10 with the appropraite operator
+# num = 6
+# print(num+4)
+
+# # Q3. Compare all variables in Q1 above
+# print(fruit1==fruit2)
+# print(fruit1==fruit3)
+# print(fruit1==fruit4)
+# print(fruit2==fruit3)
+# print(fruit2==fruit4)
+# print(fruit3==fruit4)
+
+
+# Q4. Create a shopping list and check if you have 'mango' in there
+
+    
+# Q5. Chain all comparison in Q3 above with all our logical operators
