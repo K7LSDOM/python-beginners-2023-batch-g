2b918df7939e19d223c4c43cffd983714403bd75
K7LSDOM <144117444+K7LSDOM@users.noreply.github.com>
2023-10-24 12:36:28 +0100

    Create conditionals.py

---
+++ Shedrack/conditionals.py
@@ -0,0 +1,8 @@
+db = {"Books": [15, 100],
+      "Coke": [10, 150],
+      "Biscuits": [50, 50],
+      "Milks": [30, 40],
+      "Soap": [10, 130]
+      }
+print(db)
+input('please insert your choice of item:')
